834dbd3000f7670b48db7889089a6368f2c1044f
Yauheni Lebedzik <yauheni_lebedzik@epam.com>
2019-12-05 17:32:13 +0300

    Add new DSL script

---
+++ branchJobs.groovy
@@ -0,0 +1,15 @@
+def project = 'lebedik/jenkins-code-jobs'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    job(jobName) {
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+        steps {
+            sh 'ls -lha'
+        }
+    }
+}
