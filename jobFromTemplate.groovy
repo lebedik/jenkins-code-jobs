e84d8e2ea76e6c55c8a2af4fafbec9eba396ad91
Yauheni Lebedzik <yauheni_lebedzik@epam.com>
2019-12-09 15:53:25 +0300

    Switch to freeStyleJob

--- jobFromTemplate.groovy
+++ jobFromTemplate.groovy
@@ -1,11 +1,11 @@
 
-job('base_job') {
+freeStyleJob('base_job') {
     disabled true
-    logRotator(–1, 10, –1, –1)
+    logRotator("–1, 10, –1, –1")
 }
 
 
-job('job-with-template') {
+freeStyleJob('job-with-template') {
     disabled false
     steps {
         shell "echo 'Rotates just like the template!'"
