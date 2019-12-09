e723e2daa68da28ac6b79b6589657a3d5edca90b
Yauheni Lebedzik <yauheni_lebedzik@epam.com>
2019-12-09 16:53:30 +0300

    Use template for multiple jobs

--- jobFromTemplate.groovy
+++ jobFromTemplate.groovy
@@ -1,3 +1,4 @@
+// Define Base template 
 freeStyleJob('base-template-job') {
     jdk('Java 8')
     logRotator(-1, 10)
@@ -9,13 +10,18 @@ freeStyleJob('base-template-job') {
     }
 }
 
-freeStyleJob('job-with-template') {
-    disabled false
-    steps {
-        gradle('clean build')
+// Define numbers of copies
+def n = 10
+// Create Job using using Base template
+n.times {
+    freeStyleJob("job-with-template-${it}") {
+        disabled false
+        steps {
+            gradle('clean build')
+        }
+        publishers {
+            archiveArtifacts("job-dsl-plugin/build/libs/job-dsl-${it}.hpi")
+        }
+        using('base-template-job')
     }
-    publishers {
-        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
-    }
-    using('base-template-job')
 }
