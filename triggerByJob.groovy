7175c9d2ef9c428e7b8ca9cad975da1c54ecf63c
Yauheni Lebedzik <yauheni_lebedzik@epam.com>
2019-12-16 01:03:38 +0300

    Add downstream job info to upstream job

--- triggerByJob.groovy
+++ triggerByJob.groovy
@@ -17,21 +17,29 @@ pipelineJob('Trigger FreeStyleJobs') {
   definition {
     cps {
       script('''
-        pipeline {
-            agent any
-                stages {
-                    stage('Trigger FreeStyleJob-1') {
-                        steps {
-                            build job: './FreeStyleJob-1', parameters: [string(name: 'Trigger FreeStyleJobs', value: env.NAME)], wait: true
+            def checkjob1 = build job: './FreeStyleJob-1', wait: true
+            checklog1 = Jenkins.getInstance().getItemByFullName('FreeStyleJob-1').getBuildByNumber(checkjob.getNumber()).log
+            println checklog1
+
+            def checkjob2 = build job: './FreeStyleJob-2', wait: true
+            checklog2 = Jenkins.getInstance().getItemByFullName('FreeStyleJob-2').getBuildByNumber(checkjob.getNumber()).log
+            println checklog2
+
+            pipeline {
+                agent any
+                    stages {
+                        stage('Trigger FreeStyleJob-1') {
+                            steps {
+                                build job: './FreeStyleJob-1', wait: true
+                            }
                         }
-                    }
-                    stage('Trigger FreeStyleJob-2') {
-                        steps {
-                            build job: './FreeStyleJob-2', parameters: [string(name: 'Trigger FreeStyleJobs', value: env.NAME)], wait: true
+                        stage('Trigger FreeStyleJob-2') {
+                            steps {
+                                build job: './FreeStyleJob-2', wait: true
+                            }
                         }
                     }
                 }
-            }
       '''.stripIndent())
       sandbox()     
     }
